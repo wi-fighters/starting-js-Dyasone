e959ebf0a7082a4d71e699a129082b035f9a670e
Dyasone <m.marhenke@gmx.de>
2019-08-13 10:56:52 +0200

    type of check

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ let job = 'student';
 
 //3. Concatenation
 console.log(name,"is a 31 year old", job, "who lives in", city);
+
+//4. Data Types
+let length = 16.78; //number
+let lastName = "Johnson"; //text string
+let car = ""; //undefined  but it's a string
+let isOpen = true;//boolean
+console.log(typeof length);
+console.log(typeof lastName);
+console.log(typeof car);
+console.log(typeof isOpen);

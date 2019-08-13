567c83f92e759f1973aeafc150d08ac0b95d7b4a
Dyasone <m.marhenke@gmx.de>
2019-08-13 15:01:23 +0200

    add/fixed missing const

--- Aritmetik/index.js
+++ Aritmetik/index.js
@@ -2,13 +2,15 @@
 console.log(17/30*100+"%");
 
 //2. Working Hours
-NoemyWork = (8+6+5+9+8+2+1+8.5+7+4)/10;
+const NoemyWork = (8+6+5+9+8+2+1+8.5+7+4)/10;
 console.log ("Noemy's avarage hour "+NoemyWork);
 
 //score
-lastTest = (85*6)-(75+70+85+90+100)
+const lastTest = (85*6)-(75+70+85+90+100)
 console.log("Score in the sixth test: "+ lastTest);
 
 //minimum score
-James = ((80/100*9)-(78/100*8))*100;
+const James = ((80/100*9)-(78/100*8))*100;
 console.log("James needs a minimum of "+James+"%"+" to get an 80% average");
+
+//Consumer Formula

b35715eec5c185e64a3cc9fbb3b54c4b0e129647
Dyasone <m.marhenke@gmx.de>
2019-08-14 10:35:10 +0200

    add studen  discount check

--- Ternary/index.js
+++ Ternary/index.js
@@ -9,3 +9,14 @@ const speedLimit = 130;
 const currentSpeed = 120;
 const speedCheck = (currentSpeed>speedLimit)? "You're going too fast - slow down!" :  "You're driving below the speed limit"; 
 console.log(speedCheck);
+
+//How old are you
+const drinkingAge = 21;
+const age3 = 20;
+const personAge = (age3>=drinkingAge)?"Here's some wine":"Here's some juice";
+console.log(personAge);
+
+//Student Discount
+const studentA = false;
+const isStudent =  (studentA==true) ?"Ticket costs 5,00€":"Ticket cost €12";
+console.log(isStudent);

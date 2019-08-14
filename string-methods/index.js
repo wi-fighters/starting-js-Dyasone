be82a66c925c085e585a51269d5e90eedf7a2449
Dyasone <m.marhenke@gmx.de>
2019-08-14 14:53:32 +0200

    add all comments

--- string-methods/index.js
+++ string-methods/index.js
@@ -4,3 +4,27 @@ console.log(stringLenght.length);
 const string2 = stringLenght[2];
 const string6 = stringLenght[6];
 console.log(`${string2} second letter ${string6} sixed letter`);
+
+//JS 
+
+
+//I can...
+
+//aSc
+
+//Upper Case
+
+//Lower Case
+
+//Does it contain...?
+
+//Front & Back
+
+//last Three Chars
+
+//First and Last
+
+//Concatenation
+
+//Capitalize
+

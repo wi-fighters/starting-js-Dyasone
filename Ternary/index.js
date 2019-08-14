f2c0b26fbb8b784229d6f94b09b1ae4e0b7a9100
Dyasone <m.marhenke@gmx.de>
2019-08-14 10:11:36 +0200

    add speed check

--- Ternary/index.js
+++ Ternary/index.js
@@ -3,3 +3,9 @@ const age = 18;
 const age2 = 30;
 const canDrive = (age2>=age)?"You'are  old enough, good on you" : "Too young, sorry, grow up!";
 console.log(canDrive);
+
+//Slow Down
+const speedLimit = 130;
+const currentSpeed = 120;
+const speedCheck = (currentSpeed>speedLimit)? "You're going too fast - slow down!" :  "You're driving below the speed limit"; 
+console.log(speedCheck);

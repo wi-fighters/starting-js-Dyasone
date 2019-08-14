540e5c45ac9c16d07753d1e91874a9722fba8198
Dyasone <m.marhenke@gmx.de>
2019-08-14 14:33:53 +0200

    add letter count

---
+++ string-methods/index.js
@@ -0,0 +1,6 @@
+// Characters & Properties
+const stringLenght = "wee have so much fun";
+console.log(stringLenght.length);
+const string2 = stringLenght[2];
+const string6 = stringLenght[6];
+console.log(`${string2} second letter ${string6} sixed letter`);

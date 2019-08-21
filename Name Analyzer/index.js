7cfbee8c9d3a5c484ec933f1f088e9d46f839a30
Dyasone <m.marhenke@gmx.de>
2019-08-21 09:32:11 +0200

    i m so happz it works now

--- Name Analyzer/index.js
+++ Name Analyzer/index.js
@@ -1,14 +1,17 @@
 //Name Analyzer
 //Use string methods to complete the task!
-const name = "Fran lalala ";
+const name = "Franzi Banger";
 const nameCountSpaces= name.replace(/\s/g, "");
 const nameCount= nameCountSpaces.length;
 const nameFirstLetter = name[0];
 const nameLastLetter = nameCountSpaces[nameCountSpaces.length-1];
 //const namePart = name.substring(nameCountSpaces0,nameCountSpaces.length[-3];
 const nameEndingLetters = nameCountSpaces.substring(nameCountSpaces.length-3);
+const namePartEnd = nameEndingLetters.length;
+const nameFunnyCount = nameCountSpaces.length - nameEndingLetters.length;
+const nameMix = name.substring(0,nameFunnyCount);
 const funnyNames = "chmoo";
-const nameChange = nameEndingLetters+funnyNames;
+const nameChange = nameMix+funnyNames;
 console.log(`Hello ${name.trim()}.
 Your name is ${nameCount} letters long.
 The first letter of your name is ,${nameFirstLetter} the last is ${nameLastLetter}.

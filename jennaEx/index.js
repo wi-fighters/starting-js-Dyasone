61c9f48933fb7d0c96c240674e2121ad0df4e74d
Dyasone <m.marhenke@gmx.de>
2019-08-26 15:37:06 +0200

    the amazing coffe function added

--- jennaEx/index.js
+++ jennaEx/index.js
@@ -1,5 +1,5 @@
-let coffee = 1;
-let daysBeforeExam = 10;
+const coffee = 1;
+const daysBeforeExam = 10;
 function jenna() {
     let name = "karma";
     console.log(`Schm${name}`);
@@ -19,9 +19,20 @@ function jenna() {
             -- days;
             cups, days; 
         }
+        if (days !== 0) {
+            exam(cups,days);
+        }
         console.log(`${name} drinks ${cups} coups of coffee with ${days} days before exam`);
 
     }
     exam(coffee, daysBeforeExam);
+    const schmozzler = "Schm";
+    function contact() {
+        console.log(schmozzler);
+    }
+    contact();
 }
 jenna();
+
+
+

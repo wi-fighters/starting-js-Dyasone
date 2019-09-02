6865faf85dbc4156cfd639354b4383f84d75f5a5
Dyasone <m.marhenke@gmx.de>
2019-09-02 13:48:16 +0200

    add onlz one return statment

--- franciscoMe/clothingColor.js
+++ franciscoMe/clothingColor.js
@@ -2,19 +2,17 @@
 let arrClothingColor = [];
 
 function clothingColor(color, food) {
+    const result = `The ${color} ${food}!`;
     if (color[0] === color[0].toLowerCase()) {
         color = color[0].toUpperCase() + color.substring(1);      
     }
     if (food[0] === food[0].toLowerCase()){
         food = food[0].toUpperCase() + food.substring(1);
-        arrClothingColor.push([color, food]);
-        var result = `The ${color} ${food}!`;
-    } else {
-        var result = `The ${color} ${food}!`;
+        arrClothingColor.push([color, food]);   
     }
     return result;
 }
 
-console.log(clothingColor('Red', 'carrots'));
+console.log(clothingColor('ReD', 'carrots'));
 console.log(clothingColor('Blue', 'cake'));
 console.log(clothingColor('white', 'steak'),'\nUser Inputs:',arrClothingColor);

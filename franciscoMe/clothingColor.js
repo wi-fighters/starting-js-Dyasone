ec0dc7c4d25cea1a3040c9faea6b60202d66911e
Dyasone <m.marhenke@gmx.de>
2019-09-02 14:12:24 +0200

    quick save add 2 new functions not workingnode clothingColor.js !

--- franciscoMe/clothingColor.js
+++ franciscoMe/clothingColor.js
@@ -13,6 +13,18 @@ function generateBandName(clothingColour, lastFoodEaten) {
     return result;
 }
 
+function capitalizeColor(clothingColour){
+    if (clothingColour.substring[1]=== clothingColour.substring[1].toUpperCase()){
+        clothingColour = clothingColour.substring[1].toLowerCase();
+        }
+}
+function capitalizeFood(lastFoodEaten){
+        if (lastFoodEaten.substring[1]=== lastFoodEaten.substring[1].toUpperCase()){
+            lastFoodEaten = lastFoodEaten.substring[1].toLowerCase();
+        }
+    }
+
+
 console.log(generateBandName('ReD', 'carrots'));
 console.log(generateBandName('Blue', 'cake'));
 console.log(generateBandName('white', 'steak'),'\nUser Inputs:',arrClothingColor);
